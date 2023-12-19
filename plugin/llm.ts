ade556968d236e88d03a1d76225d552b7bedc9e3
lucagrippa <lucagrip@yahoo.com>
2023-12-18 23:07:40 -0500

    Add LLM initialization and generate tags functionality

--- plugin/llm.ts
+++ plugin/llm.ts
@@ -1,27 +1,24 @@
-import { getAllTags } from 'obsidian';
-import { LLMChain } from "langchain/chains";
-
-
+import { getAllTags, Notice } from 'obsidian';
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
-
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import {
-  ChatPromptTemplate,
-  SystemMessagePromptTemplate,
-  HumanMessagePromptTemplate,
-} from "langchain/prompts";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
+import { RunnableSequence } from 'langchain/dist/schema/runnable';
+import {
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+} from "langchain/prompts";
 
-async function getVaultTags() {
+
+function getVaultTags() {
     const tagsSet = new Set(); // Use a set to ensure unique tags
 
-    const files = await this.app.vault.getMarkdownFiles();
+    const files = this.app.vault.getMarkdownFiles();
     for (const file of files) {
         const cache = this.app.metadataCache.getFileCache(file)
         const tags = getAllTags(cache);
         if (tags !== null) {
-            console.log('Tags:', tags);
             for (const tag of tags) {
                 tagsSet.add(tag);
             }
@@ -31,157 +28,143 @@ async function getVaultTags() {
     return tagsSet;
 }
 
-const systemMessage = `
-Categorize the following document based on its content. Your task is to generate relevant tags or categories that accurately represent the main themes and subject matter discussed in the document. Please provide tags or categories that best describe the content.
-
-Existing Tags/Categories:
-{tagsString}
-
-Use existing tags if they are relevant otherwise create new ones. Ensure that the tags accurately reflect the article's primary focus and themes. Return 10 tags or categories as a JSON list.
-
-Document:
-"I'm working on a web application project called "TaskMaster." TaskMaster is a task management tool designed to streamline productivity and collaboration within teams. The application allows users to create tasks, assign them to team members, set due dates, and monitor progress. It also includes real-time chat and file sharing features to facilitate communication. TaskMaster is built using the MERN stack (MongoDB, Express.js, React, Node.js) and integrates with third-party APIs for enhanced functionality."
-
-Tags:
-["#TaskManagement", "#WebApplication", "#TeamCollaboration", "#MERNStacl", "#ProductivityTools"]
-
-Document:
-"Today was a well-rounded day that began with an early trip to the gym. The workout was invigorating and set a positive tone for the day. Afterward, I enjoyed a wholesome breakfast and some quality time with a good book. In the afternoon, I ventured into the nearby park for a relaxing walk, taking in the beauty of nature. The evening was spent catching up with friends over a virtual gathering, sharing laughter and stories."
-
-Tags:
-["#Gym", "#Fitness", "#Reading", "#Nature", "#Socializing"]
-`
-
-const humanMessage = "Document: \n \"{document}\" \n Tags: \n"
-
 // write a class to instantiate the chain and handle the prompts
 export class LLM {
-    chain: LLMChain;
-    constructor(modelName: string, openAIApiKey: string) {
-        const chat = new ChatOpenAI({
-            temperature: 0,
-            modelName: modelName,
-            openAIApiKey: openAIApiKey
-        });
-        const chatPrompt = ChatPromptTemplate.fromMessages([
-            ["system", systemMessage],
-            ["human", humanMessage],
-        ]);
+    modelName: string;
+    chain: RunnableSequence;
 
-        this.chain = new LLMChain({
-            prompt: chatPrompt,
-            llm: chat,
-        });
+    constructor(modelName: string, openAIApiKey: string) {
+        this.modelName = modelName;
+        const prompt = this.getPrompt();
+        const functionCallingModel = this.getModel(modelName, openAIApiKey);
+
+        const outputParser = new JsonOutputFunctionsParser();
+        this.chain = prompt.pipe(functionCallingModel).pipe(outputParser);
     }
 
-    async createTags(text: string) {
+    getPrompt() {
+        const systemMessage = `
+    Tags are used to categorize and organize documents based on its content. Here are some existing tags that the user has created.
+
+    EXISTING TAGS:
+    {tagsString}
+
+    Tag the following document based on its content. You can use up to 5 existing tags and if necessary create up to 3 new tags. Ensure that the tags accurately reflect the article's primary focus and themes.
+    `
+
+        const humanMessage = "DOCUMENT: \n \"{document}\" \n TAGS: \n"
+
+        const prompt = new ChatPromptTemplate({
+            promptMessages: [
+                SystemMessagePromptTemplate.fromTemplate(systemMessage),
+                HumanMessagePromptTemplate.fromTemplate(humanMessage),
+            ],
+            inputVariables: ["tagsString", "document"],
+        });
+
+        return prompt;
+    }
+
+    getModel(modelName: string, openAIApiKey: string) {
+        const zodSchema = z.object({
+            tags: z.array(z.string()).describe("An array of tags that best describes the text using existing tags."),
+            newTags: z.array(z.string()).describe("An array of tags that best describes the text using new tags."),
+        });
+        const llm = new ChatOpenAI({
+            temperature: 0,
+            modelName: modelName,
+            openAIApiKey: openAIApiKey,
+        });
+
+        // Binding "function_call" below makes the model always call the specified function.
+        // If you want to allow the model to call functions selectively, omit it.
+        const functionCallingModel = llm.bind({
+            functions: [
+                {
+                    name: "output_formatter",
+                    description: "Should always be used to properly format output",
+                    parameters: zodToJsonSchema(zodSchema),
+                },
+            ],
+            function_call: { name: "output_formatter" },
+        });
+
+        return functionCallingModel;
+    }
+
+    formatTagsString(tags: string[], newTags: string[]) {
+        let tagsString = "tags: "
+        tags.forEach((tag: string) => {
+            tagsString += tag + " "
+        });
+        tagsString += "\nnew tags: "
+        newTags.forEach((tag: string) => {
+            tagsString += tag + " "
+        });
+        tagsString += "\n"
+
+        return tagsString
+    }
+
+    isTextWithinLimit(text: string, modelName: string) {
+        // Define token limits for the models
+        const tokenLimitGPT4 = 8192;
+        const tokenLimitGPT35Turbo = 4096;
+
+        // Calculate the number of tokens based on average token length (4 characters)
+        const numTokens = text.length / 4;
+
+        // Check the model name and compare with the corresponding token limit
+        if (modelName === 'gpt-4') {
+            return numTokens <= tokenLimitGPT4;
+        } else if (modelName === 'gpt-3.5-turbo') {
+            return numTokens <= tokenLimitGPT35Turbo;
+        } else {
+            // Handle unsupported models or provide a default behavior
+            console.error('Unsupported model:', modelName);
+            return false;
+        }
+    }
+
+    async generateTags(documentText: string) {
+        console.log("Generating tags...")
+        new Notice("Generating tags...")
+
+        // check if the text is within the limit
+        if (!this.isTextWithinLimit(documentText, this.modelName)) {
+            throw new Error('Your document is too long. Please reduce the length of your document.');
+        }
+
         // get every tag in the current vault
-        let vaultTags = await getVaultTags()
-        console.log(vaultTags)
+        let vaultTags = getVaultTags()
         // create a string of tags to insert into the prompt as a list
         let tagsString = ""
         vaultTags.forEach(tag => {
             tagsString += "- " + tag + "\n"
         })
-        console.log(tagsString)
 
         try {
-            const response = await this.chain.call({
+            const response = await this.chain.invoke({
                 tagsString: tagsString,
-                document: text,
+                document: documentText,
             });
-    
-            // parse the text as JSON
-            const jsonObject = JSON.parse(response.text);
-            console.log("jsonObject");
-            console.log(jsonObject);
-    
-            let tags = "";
-            jsonObject.forEach((tag: string) => {
-                tags += tag + ", ";
-            });
-            console.log(tags);
-    
-            return tags.slice(0, -2) + "\n";
+
+            const tags = this.formatTagsString(response.tags, response.newTags)
+            return tags
         } catch (error) {
-            console.error('Error parsing tags:', error);
-            throw error; // Optional: Rethrow the error to handle it further up the call stack.
+            // print the type of error
+            console.error('Error while invoking chain:', error.message);
+            if (error.message.includes('Incorrect API key')) {
+                // Notify the user about the incorrect API key
+                throw new Error('Incorrect API key. Please check your API key.');
+            }
+            else if (error.message.includes('Please reduce the length')) {
+                // Notify the user about the incorrect API key
+                throw new Error('Your document is too long. Please reduce the length of your document.');
+            } else {
+                throw new Error('Error while generating tags.');
+            }
+
         }
     }
 }
-
-
-
-
-const zodSchema = z.object({
-  tags: z.array(z.string()).describe("An array of tags that best describes the text using existing tags."),
-  newTags: z.array(z.string()).describe("An array of tags that best describes the text using new tags."),
-});
-
-const systemMessage = `
-Tags are used to categorize and organize documents based on its content. Here are some existing tags that the user has created.
-
-EXISTING TAGS:
-{tagsString}
-
-Tag the following document based on its content. You can use up to 5 existing tags and if necessary create up to 3 new tags. Ensure that the tags accurately reflect the article's primary focus and themes.
-`
-
-const humanMessage = "DOCUMENT: \n \"{document}\" \n TAGS: \n"
-
-const prompt = new ChatPromptTemplate({
-  promptMessages: [
-    SystemMessagePromptTemplate.fromTemplate(systemMessage),
-    HumanMessagePromptTemplate.fromTemplate(humanMessage),
-  ],
-  inputVariables: ["document"],
-});
-
-const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo-0613", temperature: 0 });
-
-// Binding "function_call" below makes the model always call the specified function.
-// If you want to allow the model to call functions selectively, omit it.
-const functionCallingModel = llm.bind({
-  functions: [
-    {
-      name: "output_formatter",
-      description: "Should always be used to properly format output",
-      parameters: zodToJsonSchema(zodSchema),
-    },
-  ],
-  function_call: { name: "output_formatter" },
-});
-
-const outputParser = new JsonOutputFunctionsParser();
-
-const chain = prompt.pipe(functionCallingModel).pipe(outputParser);
-
-const response = await chain.invoke({
-  document: "I like apples, bananas, oxygen, and french fries.",
-});
-
-console.log(JSON.stringify(response, null, 2));
-
-/*
-  {
-    "output": {
-      "foods": [
-        {
-          "name": "apples",
-          "healthy": true,
-          "color": "red"
-        },
-        {
-          "name": "bananas",
-          "healthy": true,
-          "color": "yellow"
-        },
-        {
-          "name": "french fries",
-          "healthy": false,
-          "color": "golden"
-        }
-      ]
-    }
-  }
-*/
--- plugin/main.ts
+++ plugin/main.ts
@@ -21,6 +21,7 @@ const DEFAULT_SETTINGS: Partial<AiPluginSettings> = {
 
 export default class AiPlugin extends Plugin {
 	settings: AiPluginSettings;
+	llm: LLM;
 
 	// this function retrieves data from disk
 	async loadSettings() {
@@ -33,166 +34,68 @@ export default class AiPlugin extends Plugin {
 	async saveSettings() {
 		// loadData() and saveData() provide an easy way to store and retrieve data from disk.
 		await this.saveData(this.settings);
+		this.llm = new LLM(this.settings.model, this.settings.openai_api_key);
 	}
 
-	
+
 	async onload() {
 		// load settings on plugin load
 		await this.loadSettings();
-		console.log(this.settings)
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new AiPluginSettingTab(this.app, this));
 
 		// instantiate LLM class
-		// const llm = new LLM(this.settings.model, this.settings.openai_api_key);
+		this.llm = new LLM(this.settings.model, this.settings.openai_api_key);
 
-		// // this adds an editor command that can translate the current selection
-		// this.addCommand({
-		// 	id: 'generate-tags',
-		// 	name: 'Generate Tags',
-		// 	editorCallback: async (editor: Editor, view: MarkdownView) => {
-		// 		try {
-		// 			// console.log(editor.getSelection());
-		// 			// let selection = editor.getSelection();
+		// This creates an icon in the left ribbon.
+		this.addRibbonIcon('wand-2', 'Generate tags!', async () => {
+			// Called when the user clicks the icon.
+			// const { editor: Editor } = this.app.workspace.getActiveViewOfType(MarkdownView);
+			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (markdownView) {
+				try {
+					const value = markdownView.editor.getValue();
 
-		// 			// // check if selection is empty and return a notice if it is
-		// 			// if (selection === "") {
-		// 			// 	new Notice("Please select some text to generate tags for.");
-		// 			// 	return;
-		// 			// }
+					const response = await this.llm.generateTags(value);
 
-		// 			let value = editor.getValue();
-		// 			console.log(value);
-		// 			let response = await llm.createTags(value);
-		// 			// editor.replaceSelection(response);
-		// 			// write the responses to the top of the document in the editor
-		// 			editor.replaceRange(response, { line: 0, ch: 0 });
-		// 		} catch (error) {
-		// 			console.error('Error while generating:', error);
-		// 		}
-		// 	}
-		// });
-
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('wand-2', 'Generate tags!', async (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	// const { editor: Editor } = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 	const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 	if (markdownView) {
-		// 		try {
-		// 			let value = markdownView.editor.getValue();
-		// 			console.log(value);
-		// 			let response = await llm.createTags(value);
-		// 			markdownView.editor.replaceRange(response, { line: 0, ch: 0 });
-		// 		} catch (error) {
-		// 			console.error('Error while generating:', error);
-		// 		}
-		// 	}
-		// });
-
-		// this.registerEvent(
-		// 	this.app.workspace.on("file-menu", (menu, file) => {
-		// 		menu.addItem((item) => {
-		// 			item
-		// 				.setTitle("Print file path 👈")
-		// 				.setIcon("document")
-		// 				.onClick(async () => {
-		// 					new Notice(file.path);
-		// 				});
-		// 		});
-		// 	})
-		// );
-
-		// this.registerEvent(
-		// 	this.app.workspace.on("editor-menu", (menu, editor, view) => {
-		// 		menu.addItem((item) => {
-		// 			item
-		// 				.setTitle("Print file path 👈")
-		// 				.setIcon("document")
-		// 				.onClick(async () => {
-		// 					new Notice(view.file.path);
-		// 				});
-		// 		});
-		// 	})
-		// );
-
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		let selection = editor.getSelection();
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+					// write the responses to the top of the document in the editor
+					markdownView.editor.replaceRange(response, { line: 0, ch: 0 });
+				} catch (error) {
+					new Notice(error.message);
+					console.error('Error while generating tags:', error);
+				}
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this registers a function to run continuously every 5 minutes
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		// this adds an editor command that can generate tags for the current selection
+		this.addCommand({
+			id: 'generate-tags',
+			name: 'Generate tags',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				try {
+					let selection = editor.getSelection();
+
+					if (selection === "") {
+						// if selection is empty, use the entire document
+						selection = editor.getValue();
+					}
+
+					let response = await this.llm.generateTags(selection);
+
+					// write the responses to the top of the document in the editor
+					editor.replaceRange(response, { line: 0, ch: 0 });
+				} catch (error) {
+					new Notice(error.message);
+					console.error('Error while generating tags:', error);
+				}
+			}
+		});
 	}
 
 	onunload() {
 
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
