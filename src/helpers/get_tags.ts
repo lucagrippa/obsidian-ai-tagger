aa1753598397a051150d7757ff6ce2f5161ae221
lucagrippa <lucagrip@yahoo.com>
2024-04-28 14:33:42 -0400

    add type hinting and improve readability

--- src/helpers/get_tags.ts
+++ src/helpers/get_tags.ts
@@ -1,18 +1,33 @@
-import { getAllTags } from 'obsidian';
+import { getAllTags, TFile, CachedMetadata} from 'obsidian';
 
-export function getVaultTags() {
-    const tagsSet = new Set(); // Use a set to ensure unique tags
+export function getVaultTags(): Array<string> {
+    const tagsSet: Set<string> = new Set(); // Use a set to ensure unique tags
 
-    const files = this.app.vault.getMarkdownFiles();
-    for (const file of files) {
-        const cache = this.app.metadataCache.getFileCache(file)
-        const tags = getAllTags(cache);
-        if (tags !== null) {
-            for (const tag of tags) {
-                tagsSet.add(tag);
+    const files: Array<TFile> = this.app.vault.getMarkdownFiles();
+    // get tags for each file
+    files.forEach((file: TFile) => {
+        const cache: CachedMetadata | null = this.app.metadataCache.getFileCache(file);
+        if (cache !== null) {
+            const tags: Array<string> | null = getAllTags(cache);
+
+            if (tags !== null) {
+                tags.forEach((tag) => tagsSet.add(tag));
             }
         }
-    }
+    })
 
-    return tagsSet;
+    const uniqueTagsArray: Array<string> = [...tagsSet];
+
+    return uniqueTagsArray;
+}
+
+export function getTagsString(): string {
+    // get every tag in the current vault
+    const vaultTags: Array<string> = getVaultTags()
+    console.log("All Vault Tags: ", vaultTags.length)
+
+    // create a string of the first 100 tags to insert into the prompt
+    const tagsString: string = vaultTags.slice(0, 100).map(tag => `- ${tag}`).join("\n");
+
+    return tagsString
 }
